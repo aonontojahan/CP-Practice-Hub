dd1b910a80b213d18244554f386ae6edf9863090
aonontojahan <aonontojahan@gmail.com>
2026-02-03 22:00:31 +0600

    Division

---
+++ Basics/Day 8/Problem2.py
@@ -0,0 +1,22 @@
+def divide_numbers():
+    try:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+
+        result = a / b
+        print("Result:", result)
+
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+
+    except ValueError:
+        print("Error: Please enter valid integers.")
+
+    except Exception as e:
+        print("Unexpected error:", e)
+
+    finally:
+        print("Program execution completed.")
+
+
+divide_numbers()
