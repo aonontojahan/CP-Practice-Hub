87e7b4947d53afbf8c0d639c089734c0122f5955
aonontojahan <aonontojahan@gmail.com>
2026-02-18 22:58:03 +0600

    Binary Seach Initially

--- Basics/Day 12/Problem1.py
+++ Basics/Day 12/Problem1.py
@@ -6,5 +6,15 @@ x = int(input())
 
 left = 0
 right = n - 1
+found_index = -1
 
+while left <= right:
+
+    mid = (left + right) // 2
+
+    print("Left:", left)
+    print("Right:", right)
+    print("Mid:", mid)
+    print("Mid Value:", arr[mid])
+    print("------------------------")
 
