2e39a9891543044c06955276c9d2620b0affd686
aonontojahan <aonontojahan@gmail.com>
2026-02-18 23:02:05 +0600

    Binary Search on Answer

--- Basics/Day 12/Problem2.py
+++ Basics/Day 12/Problem2.py
@@ -1,1 +1,44 @@
 print("Starting the program...")
+n = int(input())
+
+weights = list(map(int, input().split()))
+
+days = int(input())
+
+# Search space
+left = max(weights)
+right = sum(weights)
+
+answer = right
+
+while left <= right:
+
+    mid = (left + right) // 2
+
+    print("Trying Capacity:", mid)
+
+    required_days = 1
+    current_load = 0
+
+    i = 0
+    while i < n:
+
+        if current_load + weights[i] <= mid:
+            current_load = current_load + weights[i]
+        else:
+            required_days = required_days + 1
+            current_load = weights[i]
+
+        i = i + 1
+
+    print("Days Needed:", required_days)
+
+    if required_days <= days:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+    print("------------------------")
+
+print("Minimum Required Capacity:", answer)
