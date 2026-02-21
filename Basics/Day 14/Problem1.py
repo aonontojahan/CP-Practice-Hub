cb881933466d5524167cef88ea65e6c775faa007
aonontojahan <aonontojahan@gmail.com>
2026-02-22 03:22:09 +0600

    Backtracking Logic building

--- Basics/Day 14/Problem1.py
+++ Basics/Day 14/Problem1.py
@@ -1,2 +1,23 @@
 print("practicing CP Day 14")
 print("Practicing BACKTRACKING Related Problems")
+
+def generate_subsets(index, current, n):
+
+    if index > n:
+        print(current)
+        return
+
+    # Choice 1: Include index
+    current.append(index)
+    generate_subsets(index + 1, current, n)
+
+    # Backtrack
+    current.pop()
+
+    # Choice 2: Exclude index
+    generate_subsets(index + 1, current, n)
+
+
+n = int(input())
+
+generate_subsets(1, [], n)
