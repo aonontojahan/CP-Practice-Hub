c814db307ceac1e1d800bf9ff75373d758f4e72b
aonontojahan <aonontojahan@gmail.com>
2026-02-21 22:46:27 +0600

    Recursion Problem Solved Version

--- Basics/Day 13/Problem1.py
+++ Basics/Day 13/Problem1.py
@@ -1,3 +1,29 @@
 print("Hello Everyone")
 print("Practicing CP Day 13")
 print("Practicing Recursion Related Problems")
+
+def factorial(n):
+
+    print("Calling factorial with:", n)
+
+    # Base case
+    if n == 0:
+        print("Base case reached")
+        return 1
+
+    if n == 1:
+        print("Base case reached")
+        return 1
+
+    # Recursive case
+    result = n * factorial(n - 1)
+
+    print("Returning:", result)
+
+    return result
+
+
+num = int(input())
+
+answer = factorial(num)
+
